53b142ed4d65ae897b7927c2fb012e25365826f0
Ivan Braun <ivan@visualpharm.com>
2025-06-04 13:47:07 -0300

    updates docker

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Enable hot reloading in Docker
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+  // Ensure the app binds to all interfaces
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./lib/**/*'],
+    },
+  },
 }
 
 export default nextConfig
